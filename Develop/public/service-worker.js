35255014721c12d681d56d5b3d3061f43e34b88c
Brandon Conte <bconte950@gmail.com>
2022-02-12 13:08:33 -0500

    Activate service worker

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -2,6 +2,7 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
+
 const FILES_TO_CACHE = [
     './index.html',
     './css/styles.css',
@@ -18,6 +19,7 @@ const FILES_TO_CACHE = [
     './icons/icon-72x72.png'
 ];
 
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -25,4 +27,23 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-}); 
+});
+
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
