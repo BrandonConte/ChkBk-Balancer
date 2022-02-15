61d294975b72e27011ae64efd9e63b47db07039b
Brandon Conte <bconte950@gmail.com>
2022-02-15 18:48:58 -0500

    Added mongoDB connection to Heroku

--- server.js
+++ server.js
@@ -16,10 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/ChkBkBalancer',
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
+);
 
 // routes
 app.use(require("./routes/api.js"));
